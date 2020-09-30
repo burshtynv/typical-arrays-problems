3c891a19490f08afe92b1bc24bce85c0db43404e
burshtynv <vanyaburshtyn@gmaol.com>
2020-09-30 13:16:53 +0300

    add function min max avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
+  if(array = undefined || array === 0)
   return 0;
+  var min = array[0];
+  for(i = 1; i < array.length; i++){
+    if(min > array[i])
+    min = array[i];
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array = undefined || array === 0)
+  return 0; 
+  var max = array[0];
+  for(i = 1; i < array.length; i++)
+  if(max < array[i]) 
+  array[i] = max
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array = undefined || array === 0)
+  return 0; 
+  var sum = 0;
+  for(i = 1; i < array.length; i++)
+  sum = sum + array[i];
 }
+return sum / array.length;
